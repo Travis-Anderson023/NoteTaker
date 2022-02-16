231380bc12a8d00030cc3374af5674579063e44e
Spotexx <tsanderson.023@gmail.com>
2022-02-16 18:13:50 -0500

    set up express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//serve public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/views/notes.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
