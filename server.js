3f3161189fb333a81a95e7d7d3e1acb75152a635
Spotexx <tsanderson.023@gmail.com>
2022-02-16 21:30:22 -0500

    left side shows up next is to hook up ids

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //serve public folder
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -13,6 +17,44 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
+
+app.post('/api/notes', (req, res) => {
+    //saves note to db
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
+        const oldNotes = JSON.parse(data);
+        const newNote = req.body;
+        oldNotes.push(newNote);
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(oldNotes), (err) => {
+            if (err) throw err;
+        });
+    });
+});
+
+app.get('/api/notes', (req, res) => {
+    //gets all notes from db
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    //deletes note from db
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
+        const oldNotes = JSON.parse(data);
+        const noteId = req.params.id;
+        const newNotes = oldNotes.filter(note => note.id !== noteId);
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(newNotes), (err) => {
+            if (err) throw err;
+        });
+    });
+});
+
+    
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
